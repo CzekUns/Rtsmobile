37408aa027f75cbf0b28b309291a9de4337008f8
CzekUns <132501977+CzekUns@users.noreply.github.com>
2026-09-15 23:22:37 +0200

    Refresh cache for mouse-like touch controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='terra-italica-v4';
+const CACHE='terra-italica-v5';
 const ASSETS=['./','./index.html','./app.css','./game.js','./touch-modifier.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
