46ef1940dacfc42db8808c1e4275da0e7e6bcd8d
CzekUns <132501977+CzekUns@users.noreply.github.com>
2026-09-16 21:52:01 +0200

    Cache production building assets

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-const CACHE='terra-italica-v7';
+const CACHE='terra-italica-v8';
 const ASSETS=[
   './','./index.html','./app.css','./game.js','./touch-modifier.js','./p16-render.js','./asset-render.js','./manifest.webmanifest',
   './assets/p16/units/unit_human_p16.png','./assets/p16/units/unit_raider_p16.png',
   './assets/p16/animals/animal_sheep_p16.png','./assets/p16/animals/animal_wolf_p16.png',
   './assets/p16/resources/resource_tree_p16.png','./assets/p16/resources/resource_stone_p16.png',
-  './assets/p16/resources/resource_iron_p16.png','./assets/p16/resources/resource_berry_p16.png'
+  './assets/p16/resources/resource_iron_p16.png','./assets/p16/resources/resource_berry_p16.png',
+  './assets/p16/buildings/building_house_p16.png','./assets/p16/buildings/building_farm_p16.png',
+  './assets/p16/buildings/building_warehouse_p16.png','./assets/p16/buildings/building_tower_p16.png',
+  './assets/p16/buildings/building_palisade_p16.png'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
